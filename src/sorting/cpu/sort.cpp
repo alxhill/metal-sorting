297d4c7445b1903b9b424cc1df972a7c99609018
Alexander Hill <me@alxhill.com>
2023-10-02 23:59:32 -0400

    autocommit

--- src/sorting/cpu/sort.cpp
+++ src/sorting/cpu/sort.cpp
@@ -25,11 +25,8 @@ std::vector<unsigned int> sort_bitonic(std::vector<unsigned int> values) {
 
 void sort_radix_inplace_recur(std::vector<unsigned int> &values, const int start, const int end, unsigned int bitmask) {
     if (bitmask == 0) {
-//        std::cout << "done sorting " << start << "-" << end << std::endl;
         return;
     }
-
-//    std::cout << "sorting from " << start << "-" << end << std::endl;
 
     int zeroMark = start - 1;
     int oneMark = end + 1;
@@ -43,15 +40,6 @@ void sort_radix_inplace_recur(std::vector<unsigned int> &values, const int start
         }
     }
 
-//    std::cout << "sorted with bitmask:" << std::bitset<4>(bitmask) << std::endl;
-//    std::cout << "zeroMark: " << zeroMark << " oneMark: " << oneMark << std::endl;
-//
-//    std::cout << "ordered values:";
-//    for (int i = start; i <= end; i++) {
-//        std::cout << "[" << i << "]" << std::bitset<4>(values[i]) << " ";
-//    }
-//    std::cout << std::endl;
-
     if (zeroMark - start > 0) {
         sort_radix_inplace_recur(values, start, zeroMark, bitmask >> 1);
     }
@@ -61,17 +49,7 @@ void sort_radix_inplace_recur(std::vector<unsigned int> &values, const int start
 }
 
 void sort_radix(std::vector<unsigned int>& values) {
-//    std::cout << "initial values:";
-//    for (unsigned int i : values) {
-//        std::cout << std::bitset<4>(i) << " ";
-//    }
-//    std::cout << std::endl;
     sort_radix_inplace_recur(values, 0, values.size() - 1, 0b1 << 31);
-//    std::cout << "final values:";
-//    for (unsigned int i : values) {
-//        std::cout << std::bitset<4>(i) << " ";
-//    }
-//    std::cout << std::endl;
 }
 
 void sort_stdlib(std::vector<unsigned int>& values) {
